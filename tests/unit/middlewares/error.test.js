5c1fbecc38d9a687bf843349a09ffe34d5790d07
Daniel B. Hagen <5039466+dbhagen@users.noreply.github.com>
2023-08-28 14:19:13 -0500

    Removing __mock__ folder, unneeded, and bumping dependancies for dependabot

--- tests/unit/middlewares/error.test.js
+++ tests/unit/middlewares/error.test.js
@@ -67,22 +67,6 @@ describe('Error middlewares', () => {
       )
     })
 
-    // test('should convert a Mongoose error to ApiError with status 400 and preserve its message', () => {
-    //   const error = new mongoose.Error('Any mongoose error')
-    //   const next = jest.fn()
-
-    //   errorConverter(error, httpMocks.createRequest(), httpMocks.createResponse(), next)
-
-    //   expect(next).toHaveBeenCalledWith(expect.any(ApiError))
-    //   expect(next).toHaveBeenCalledWith(
-    //     expect.objectContaining({
-    //       statusCode: httpStatus.BAD_REQUEST,
-    //       message: error.message,
-    //       isOperational: false,
-    //     })
-    //   )
-    // })
-
     test('should convert any other object to ApiError with status 500 and its message', () => {
       const error = {}
       const next = jest.fn()
